586102b25edc3211d77f8ba75c7fe2b8eefee7da
Zac Wellsandt <mayhemphone@gmail.com>
2024-06-19 13:17:03 -0700

    show how many games

--- src/components/Average.tsx
+++ src/components/Average.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 //import styles from './Average.module.scss'
-import { getAverageScores } from "@/db/queries";
+import { getAverageScores, getGames } from "@/db/queries";
 type Props = {};
 
 const Average = async ({}: Props) => {
-  const avgScores = getAverageScores();
-  return <h1>{avgScores}</h1>;
+  const avgScores = await getAverageScores();
+  const games = await getGames();
+  return (
+    <>
+      <h1>{avgScores} average</h1>
+      <h3>from {games.length} games</h3>
+    </>
+  );
 };
 
 export default Average;
