caf711cc5e6bc96d735bb2f9f348b6a802539052
Zac Wellsandt <mayhemphone@gmail.com>
2024-08-17 19:53:45 -0700

    stopping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default async function Home() {
       <h2 className="text-4xl font-bold">Averages</h2>
       <div className="relative">
         <div className="flex  space-x-4 overflow-x-scroll pb-4">
-          <Average />
-          <Average months={3} />
           <Average months={1} />
+          <Average months={3} />
+          <Average />
         </div>
         <div className="pointer-events-none absolute -right-4 w-4/12 top-0 bottom-0 bg-gradient-to-l from-transparent lg:from-transparent"></div>
       </div>
