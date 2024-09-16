74873b840ccab0baefcfdc9a40dd16aac4b859cf
Zac Wellsandt <mayhemphone@gmail.com>
2024-09-15 22:03:50 -0700

    removed some api routes that aren't being used

--- src/components/RelatedSelect.tsx
+++ src/components/RelatedSelect.tsx
@@ -79,7 +79,6 @@ export function RelatedSelect<T extends Record<string, any>>({
               </SelectTrigger>
             </FormControl>
             <SelectContent>
-              {/* TODO: fetch these from related table */}
               {relatedData.length > 0 &&
                 relatedData.map((item, i) => (
                   <SelectItem key={i} value={item.id}>
--- src/db/queries/games.ts
+++ src/db/queries/games.ts
@@ -1,5 +1,3 @@
-// TODO - REMOVE! this exposes shit!
-
 import { GamesData } from "@/util/api/ingestGame";
 import { count, sql } from "drizzle-orm";
 import { db } from "../";
