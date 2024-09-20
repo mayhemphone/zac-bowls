0b1c41e5481b0e8c1b93fa87332a13b32363aff4
Zac Wellsandt <mayhemphone@gmail.com>
2024-09-19 22:38:07 -0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import SignedIn from "@/components/SignedIn";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
@@ -38,6 +39,7 @@ export default function RootLayout({
         )}
       >
         <SpeedInsights />
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
